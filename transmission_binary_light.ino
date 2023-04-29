b258f38aa40547ec3f19e3b1580d87de106d87d4
iDevYT <alphangred57@gmail.com>
2023-04-29 22:28:18 +0200

    init

---
+++ transmission_binary_light.ino
@@ -0,0 +1,29 @@
+#define btnPin 2
+#define ledPin 3
+#define arduinoLED LED_BUILTIN
+
+int numbers[] = {0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+int length = sizeof(numbers);
+void setup(){
+    pinMode(ledPin, OUTPUT);
+    pinMode(btnPin, INPUT);
+    pinMode(arduinoLED, OUTPUT);
+}
+
+void loop(){
+    int btnSwitch = digitalRead(btnPin);
+    if(btnSwitch == LOW){
+        digitalWrite(ledPin, LOW);
+        digitalWrite(arduinoLED, LOW);
+    }
+    else{
+        digitalWrite(arduinoLED, HIGH);
+        for (size_t i = 0; i < length; i++)
+        {
+            digitalWrite(ledPin, numbers[i]);
+            delay(200);
+        }
+        digitalWrite(arduinoLED, LOW);
+        
+    }
+}
